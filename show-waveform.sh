d4ca5df46926ef9f0fb266eba6f0b25dc9738ad3
Florian Lonsing <lonsing@cs.stanford.edu>
2019-06-03 10:50:08 -0700

    wave form script

--- show-waveform.sh
+++ show-waveform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "gtkwave ./bug-trace-k10.vcd bug-trace-gtkwave.gtkw'"
-gtkwave ./bug-trace-k10.vcd bug-trace-gtkwave.gtkw
+gtkwave ./bug-trace-k10.vcd bug-trace-gtkwave.gtkw 2>/dev/null
