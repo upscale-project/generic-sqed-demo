8ebe3386ba27b7232280ab368eff3b540372b610
Makai Mann <makaim@stanford.edu>
2020-09-14 16:46:56 -0700

    Add new patch files for generating BTOR2 with embedded assertion and constraints

--- wire-up-qed-module.sh
+++ wire-up-qed-module.sh
@@ -15,12 +15,22 @@ patch -i ./step1-topsim-changes.patch ../ridecore-src-buggy/topsim.v
 # instruction to be a valid instruction from the ISA
 patch -i ./step2-topsim-changes.patch ../ridecore-src-buggy/topsim.v
 
+# Use assumption to embed constraint that there's no reset in the BTOR2
+# reset sequence will be simulated before BTOR2 generated
+patch -i ./step3-topsim-changes.patch ../ridecore-src-buggy/topsim.v
+
 # Update the pipeline module to wire up the QED module
 
 # Drive instruction signal from top-level input, instantiate the QED
 # module to modify instructions, send the output instruction of the
 # QED module through the pipeline
 patch -i ./step0-pipeline-changes.patch ../ridecore-src-buggy/pipeline.v
+
+# wire registers 1 and 17 out of regfile so we can access them
+# in standard Verilog (Yosys doesn't support . notation for reaching into modules)
+patch -i ./step0-ram_sync_nolatch-changes.patch ../ridecore-src-buggy/ram_sync_nolatch.v
+patch -i ./step0-arf-changes.patch ../ridecore-src-buggy/arf.v
+patch -i ./step1-pipeline-changes.patch ../ridecore-src-buggy/pipeline.v
 
 # OPTIONAL: make additional modifications to speed up model checking
 
@@ -34,7 +44,7 @@ patch -i ./step0-dmem-changes-optimization.patch ../ridecore-src-buggy/dmem.v
 
 # Limit instruction fetch to only one instruction
 
-patch -i ./step1-pipeline-changes-optimization.patch ../ridecore-src-buggy/pipeline.v
+patch -i ./step2-pipeline-changes-optimization.patch ../ridecore-src-buggy/pipeline.v
 
 # Remove branch target buffer to get rid of neg-edge behavior. This
 # optimization allows to abstract the clock and reduce the number of
