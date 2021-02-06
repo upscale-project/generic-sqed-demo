364acca1bccabb6c31fd9e43b8c35c9fcb575b75
Florian Lonsing <lonsing@cs.stanford.edu>
2021-02-05 17:02:15 -0800

    pull sqed-generator repo, update scripts

--- generate-qed-files.sh
+++ generate-qed-files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd ./SQED-Generator/Generators/
+cd ./sqed-generator/SQED-Generator/Generators/
 
-python ./generate_sqed.py ../FormatFiles/RV32M-ridecore_format.txt
+python ./generate_sqed.py ../FormatFiles/RV32M-ridecore_format.txt ../QEDFiles
+
+cp ../Design_Independent_QED_Files/* ../QEDFiles
+
+cd ..
 
 cd ..
 
